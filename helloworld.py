d64f02960cf314c3475ee79a973ce575cc25778c
Bradley Rowe <bradrowesf@yahoo.com>
2016-06-23 23:47:49 -0700

    more practice

--- helloworld.py
+++ helloworld.py
@@ -5,3 +5,9 @@ print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+
+from datetime import datetime
+
+now = datetime.now()
+
+print '%s:%s:%s' % (now.hour, now.minute, now.second)
